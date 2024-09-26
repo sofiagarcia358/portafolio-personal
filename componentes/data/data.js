e21e9fcce6b98bea8ff89d58a89095bd7b80efa3
sofia <sofiagarcia.2021p@gmail.com>
2024-09-26 12:16:11 -0600

    se crea data y diseño js

---
+++ componentes/data/data.js
@@ -0,0 +1,11 @@
+let dataProyectos = [
+    {
+        nombre: "Formularios",
+        descripcion: "text texto texto",
+        web: "http://google.com",
+        githubs:"https://github.com",
+        Stacks: ("js", "css", "html")
+    }
+];
+
+export {dataProyectos}
--- componentes/proyectos/proyectos.js
+++ componentes/proyectos/proyectos.js
@@ -1,18 +1,52 @@
 import { header } from "../Header/header.js";
+import { dataProyectos } from "../data/data.js";
+
 function proyectos(){
 
     let section = document.createElement('section');
     section.className = `section-proyectos`;
     section.appendChild(header());
-    section.appendChild(item());
+    section.appendChild(listaProyectos());
 
     return section;
 }
+function listaProyectos(){
+    let div = document.createElement('div');
+    div.className = "lista-Proyectos";
+
+    dataProyectos.forEach((proyecto)=>{
+        div.appendChild( item(proyecto.nombre, proyecto.githubs, proyecto.Stacks, proyecto.web));
+    });
+
+    
+
+    return div;
+}
 
 //Item lista
-function item(){
+function item(nombre, githubs, web, Stacks){
     let div = document.createElement('div');
-    div.innerText = "item de lista";
+    div.className = "div-item";
+
+    let a = document.createElement('a');
+    a.className = "link-github-page";
+    a.href = web;
+    a.innerText = "web";
+    div.appendChild(a);
+
+
+    let Stack = document.createElement('div');
+    Stack.innerText = "[js, CSS, HTML]";
+    div.appendChild(Stack);
+
+
+    let btnGithub = document.createElement('div');
+    btnGithub.className = "btn-github";
+    btnGithub.href ="https://github.com/sofiagarcia358/";
+    btnGithub.innerText = "Github";
+    div.appendChild(btnGithub);
+
+
 
     return div;
 }
